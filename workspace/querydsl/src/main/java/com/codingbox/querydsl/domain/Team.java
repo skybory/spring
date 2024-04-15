a57770f231f6a5b1400029abe3a1083228d80767
TYKIM <xodus4194@gmail.com>
2024-04-15 15:38:20 +0900

    study 0415

---
+++ workspace/querydsl/src/main/java/com/codingbox/querydsl/domain/Team.java
@@ -0,0 +1,44 @@
+package com.codingbox.querydsl.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Team {
+
+	@Id	@GeneratedValue
+	@Column(name = "team_id")
+	private long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "team")
+	private List<Member> members = new ArrayList<>();
+	
+	
+	public Team(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Team [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+	
+	
+}
